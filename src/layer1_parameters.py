04878538426205e8a14439dcead66fee694e33ed
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-09 14:50:35 +0530

    Function for printing parameters

--- src/layer1_parameters.py
+++ src/layer1_parameters.py
@@ -16,3 +16,20 @@ def get_default_parameters():
         "v_reset":-80.0
     }
     return parameters
+
+def print_parameters(params):    # why have this function - we might want to print parameters many times
+    """
+    Print neuron parameters in a readable format.
+
+    Args:
+        params (dict): Dictionary of neuron parameters
+    """
+    print("\n" + "="*50)  # Creating a string with 50 equal signs
+    print("NEURON PARAMETERS")
+    print("="*50)
+    print(f"Membrane time constant (tau):   {params['tau']:.1f} ms")  # {params['tau] gets th evalue from dictionary
+    #:.1f formats it to a decimal place  --- eg :- 20.0 not 20.00000
+    print(f"Resting potential (v_rest):    {params['v_rest']:.1f} mV")
+    print(f"Spike threshold (v_threshold): {params['v_threshold']:.1f} mV")
+    print(f"Reset potential (v_reset):     {params['v_reset']:.1f} mV")
+    print("="*50 + "\n")
