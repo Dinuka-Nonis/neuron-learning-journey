89c5e890e6e7b8c5b82854cb74080b57bd12681d
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-09 15:29:38 +0530

    - Added create_constant_input() for steady stimulation

--- src/layer1_input.py
+++ src/layer1_input.py
@@ -15,3 +15,21 @@ def create_time_grid(dt, t_total):
     time = np.arrange(0, t_total + dt, dt)
     return time
 
+def create_constant_inputs(time, amplitude):
+    """
+    Create a constant input current.
+
+    Args:
+        time (np.ndarray): Time grid
+        amplitude (float): Current amplitude (typically in nA or pA)
+
+    Returns:
+        np.ndarray: Current values at each time point
+    """
+# Used for testing if neuron spikes with steady input
+# A simplest experiment
+# Finding minimum current needed to spike
+
+    current = np.ones_like(time)* amplitude   #creates an array of 1's . if time has 1000 points, current has 1000 points
+    return current
+# multiplied by amplitude because we want all values to be 'amplitude' , not 1
