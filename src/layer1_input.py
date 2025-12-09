e1085d3fb6ab6fd99675c5b52b6f5e6a8c3a8320
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-09 16:43:07 +0530

    - Added create_pulse_input() for transient stimulation

--- src/layer1_input.py
+++ src/layer1_input.py
@@ -33,3 +33,22 @@ def create_constant_inputs(time, amplitude):
     current = np.ones_like(time)* amplitude   #creates an array of 1's . if time has 1000 points, current has 1000 points
     return current
 # multiplied by amplitude because we want all values to be 'amplitude' , not 1
+
+def create_pulse_input(time, start_time, end_time, amplitude):
+    """
+    Create a pulse input current (on during a time window).
+
+    Args:
+        time (np.ndarray): Time grid
+        start_time (float): When pulse starts (ms)
+        end_time (float): When pulse ends (ms)
+        amplitude (float): Current amplitude during pulse
+
+    Returns:
+        np.ndarray:  Current values at each time point
+    """
+    current  = np.zeros_like(time)  # start with zeros  - same length as time
+    pulse_indices = (time >= start_time) & (time<= end_time)  # find indices where time is in range
+    #sets values to amplitude only where mask is true
+    current[pulse_indices] = amplitude   # set those time to amplitude
+    return current
