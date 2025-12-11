19dccc56b7c528dfd0632597df47dab5d1fad3db
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-11 18:29:26 +0530

    function to compute MSE only on non spike regions

--- src/layer3_loss_functions.py
+++ src/layer3_loss_functions.py
@@ -22,3 +22,36 @@ def compute_voltage_mse(simulated_voltage, target_voltage):
 
     mse = np.mean(squared_diff)
     return mse
+
+def compute_subthreshold_mse(simulated_voltage, target_voltage, params):
+    """
+    Compute MSE only on differniable parts - excluding spikes 
+     this is important because 
+     - spikes are discontinous 
+
+    Args:
+        simulated_voltage (np.ndarray): simulated voltage trace
+        target_voltage (np.ndarray): target voltage trace
+        params (dict): neuron parameters
+    """
+    v_reset = params['v_reset']
+
+    #Create a mask - true where voltage is not at reset
+    sim_mask = simulated_voltage != v_reset
+    target_mask = target_voltage != v_reset
+
+    #only include points that are sub-threshold in both traces
+    combined_mask = sim_mask& target_mask
+
+    #extract sub threshold voltages
+    sim_subthreshold = simulated_voltage[combined_mask]
+    target_subthreshold = target_voltage[combined_mask]
+
+    #compute MSE only in these points
+    if len(sim_subthreshold) == 0:
+        return 0.0
+    
+    squared_diff = (sim_subthreshold - target_subthreshold)**2
+    mse = np.mean(squared_diff)
+
+    return mse
